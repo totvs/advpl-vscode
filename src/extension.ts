f2a7760856a4447276405bb4591195b69987c765
Atila Augusto <atila.sistemas@gmail.com>
2017-07-07 15:50:33 -0300

    ajuste atalho ctrl+f11

--- src/extension.ts
+++ src/extension.ts
@@ -126,7 +126,13 @@ function startSmartClient()
 let disposable = vscode.commands.registerCommand('advpl.startSmartClient', () => {
         
         var scPath :string; 
-        scPath = vscode.workspace.getConfiguration("advpl").get<string>("smartClientPath");
+        var ambientes:Array<any> = vscode.workspace.getConfiguration("advpl").get<Array<any>>("environments");
+        var ambienteAtual:string = vscode.workspace.getConfiguration("advpl").get<string>("selectedEnvironment");
+       
+        ambientes.forEach(element => {
+            if(element.environment == ambienteAtual)
+               scPath = element.smartClientPath
+        });
        // scPath += "smartclient.exe";
         var obj2  = new smartClientLaunch(scPath);
         obj2.start();
