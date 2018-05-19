8adba371e722f9c7072e423483e33edd70a80f9a
Rodrigo Antonio <ragsweb@gmail.com>
2018-05-19 15:35:16 -0300

    Instalacao do binario no linux e ajuste no Mac

--- src/utils/debugBridge.ts
+++ src/utils/debugBridge.ts
@@ -39,19 +39,20 @@ export function getAdvplDebugBridge (){
     }    
     return path;
 }
-function runUnzip(path, args,OutPutChannel)
+function runUnzip(path, args, file ,OutPutChannel)
 {
     let ok : boolean;    
     //unzip -d ${HOME}/lib AdvtecDebugBridgeMAC.zip    
+    args.push(file);
     const child = child_process.spawn("unzip", args,{cwd:path, shell:true });
-    child.stdout.on("data", function (data) {
+    /*child.stdout.on("data", function (data) {
         OutPutChannel.log("unzip..")
         OutPutChannel.log(data);
-    });
-/*
-    child.on("exit", function (data) {
-        ok = false;
     });*/
+
+    child.on("exit", function (data) {
+        fs.unlink(path+file);
+    });
     
     return true;
 }
@@ -59,17 +60,36 @@ export function installAdvplDebugBridge (OutPutChannel){
     if (process.platform == "darwin")
     {
         let path = getAdvplBinPath(false);
-        if (!fs.existsSync("${HOME}/lib/libboost_system.dylib"))
+        //if (!fs.existsSync("${HOME}/lib/libboost_system.dylib"))
+        if (fs.existsSync(path+'AdvtecDebugBridgeMAC_bin.zip'))
+        {
+            let _args = new Array<string>();            
+            runUnzip(path,_args,'AdvtecDebugBridgeMAC_bin.zip', OutPutChannel);
+        }
+        if (fs.existsSync(path+'AdvtecDebugBridgeMAC_lib.zip'))
         {
             let _args2 = new Array<string>();
             _args2.push("-d");
-            _args2.push("${HOME}/lib");
-            _args2.push("AdvtecDebugBridgeMAC_lib.zip");
-            runUnzip(path,_args2,OutPutChannel);
+            _args2.push("${HOME}/lib");            
+            runUnzip(path,_args2,'AdvtecDebugBridgeMAC_lib.zip',OutPutChannel);
+        }
+    }
+    else
+    {
+        if (process.platform == "linux")
+        {
+            let path = getAdvplBinPath(false);
+            if (fs.existsSync(path+'AdvplDebugBridge-linux.tar.gz'))
+            {
+                let _args = new Array<string>();
+                _args.push('-vzxf');
+                _args.push('AdvplDebugBridge-linux.tar.gz');
+                const child = child_process.spawn('tar', _args,{cwd:path, shell:true });
+                child.on("exit", function (data) {
+                    fs.unlink(path+'AdvplDebugBridge-linux.tar.gz');
+                });
+            }
 
-            let _args = new Array<string>();            
-            _args.push("AdvtecDebugBridgeMAC_bin.zip");         
-            runUnzip(path,_args,OutPutChannel);
         }
     }
     
