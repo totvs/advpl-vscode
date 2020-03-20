c12cf544a8ba778c5cfc5420e11c3f0901745fd2
Robson Rogério Silva <robson_rogerio@hotmail.com.br>
2020-03-20 18:44:22 -0300

    Implementação de range format.

--- src/codeFormat/formatting.ts
+++ src/codeFormat/formatting.ts
@@ -5,27 +5,45 @@ import {
   CancellationToken,
   ProviderResult,
   TextEdit,
-  DocumentRangeFormattingEditProvider
+  DocumentRangeFormattingEditProvider,
+  Range,
+  Position
 } from 'vscode';
 import { FormattingRules, RuleMatch } from './formmatingRules';
 
+// Regras de estruturas que não sofrem identação interna
+let structsNoIdent: string[] = [
+  'beginsql (alias)?',
+  'comment',
+  'protheus doc',
+  'begin content',
+  'no format'
+];
+
 class Formatting implements DocumentFormattingEditProvider {
   lineContinue: boolean = false;
-  // Regras de estruturas que não sofrem identação interna
-  structsNoIdent: string[] = [
-    'beginsql (alias)?',
-    'comment',
-    'protheus doc',
-    'begin content',
-    'no format'
-  ];
 
   provideDocumentFormattingEdits(
     document: TextDocument,
     options: FormattingOptions,
     token: CancellationToken
   ): ProviderResult<TextEdit[]> {
-    const formattingRules = new FormattingRules();
+	const formatter: RangeFormatting = new RangeFormatting();
+	// define o range como todo o documento
+	const range: Range = new Range(new Position(0,0), new Position(document.lineCount,0)); 
+    return formatter.provideDocumentRangeFormattingEdits(document,range,options,token);
+  }
+}
+
+class RangeFormatting implements DocumentRangeFormattingEditProvider {
+  lineContinue: boolean = false;
+  provideDocumentRangeFormattingEdits(
+    document: TextDocument,
+    range: Range,
+    options: FormattingOptions,
+    token: CancellationToken
+  ): ProviderResult<TextEdit[]> {
+	const formattingRules = new FormattingRules();
     const tab: string = options.insertSpaces
       ? ' '.repeat(options.tabSize)
       : '\t';
@@ -33,14 +51,14 @@ class Formatting implements DocumentFormattingEditProvider {
     let cont: number = 0;
 
     let result: TextEdit[] = [];
-    const lc = document.lineCount;
+	const lc = range.end.line;
     const rulesIgnored: any[] = formattingRules
       .getClosedStructures()
       .filter(rule => {
-        return this.structsNoIdent.indexOf(rule.id) !== -1;
+        return structsNoIdent.indexOf(rule.id) !== -1;
       });
 
-    for (let nl = 0; nl < lc; nl++) {
+    for (let nl = range.start.line; nl < lc; nl++) {
       const line = document.lineAt(nl);
       const text = line.text.trimRight();
       let lastRule: string =
@@ -100,17 +118,6 @@ class Formatting implements DocumentFormattingEditProvider {
     }
 
     return result;
-  }
-}
-
-class RangeFormatting implements DocumentRangeFormattingEditProvider {
-  provideDocumentRangeFormattingEdits(
-    document: TextDocument,
-    range: import('vscode').Range,
-    options: FormattingOptions,
-    token: CancellationToken
-  ): ProviderResult<TextEdit[]> {
-    throw new Error('Method not implemented.');
   }
 }
 
