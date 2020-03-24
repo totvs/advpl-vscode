f73585c95ebabb27105abd0c2b902e6ebe2a820c
Gabriel Alencar <pixcel97@gmail.com>
2020-03-24 17:30:53 -0300

    Adicionado referencia da documentação da identação na extensão

--- src/vscode-whats-new/whatsNew.ts
+++ src/vscode-whats-new/whatsNew.ts
@@ -8,39 +8,54 @@ export class WhatsNewAdvPLContentProvider implements ContentProvider {
         return <Header>{
             logo: <Image>{ src: logoUrl, height: 25, width: 25 },
             message: `<b>I Love AdvPL</b> provê os recursos necessários para edição, 
-            compilação e debugging em AdvPL, 4GL e TL++ no Visual Code, além de recursos específicos
-            para gerenciamento de ambientes Protheus.`,
+                      compilação e debugging em AdvPL, 4GL e TL++ no Visual Code, além de recursos específicos
+                      para gerenciamento de ambientes Protheus.`,
             notice: `Recomenda-se em ambientes Windows alterar a configuração <code>"advpl.alpha_compile": true</code>
-                    pois as melhorias no Bridge (intermediador do VsCode com AppServer) estão sendo feitas 
-                    somente nessa versão.`};
+                     pois as melhorias no Bridge (intermediador do VsCode com AppServer) estão sendo feitas 
+                     somente nessa versão.`};
     }
 
     public provideChangeLog(): ChangeLogItem[] {
         const changeLog: ChangeLogItem[] = [];
-        
+
         changeLog.push({ kind: ChangeLogKind.NEW, message: "Adicionado <b>What's New</b> para apresentar as novidades e correções da extensão" });
+
         changeLog.push({
             kind: ChangeLogKind.NEW, message: `0.13.0 - Adicionado suporte ao <b>binário 191205P</b> - (<a title=\"Documentação para uso do binário 191205P\" 
-                    href=\"https://github.com/totvs/advpl-vscode/wiki/Suporte-ao-bin%C3%A1rio-191205P\">Documentação</a>)` });
+                href=\"https://github.com/totvs/advpl-vscode/wiki/Suporte-ao-bin%C3%A1rio-191205P\">Documentação</a>)`
+        });
+
         changeLog.push({
             kind: ChangeLogKind.NEW, message: `Implementado <b>Hover Inspect</b> no debug - (<a title=\"Open Issue #417\" 
-                href=\"https://github.com/totvs/advpl-vscode/issues/417\">Issue #417</a> | <a <a title=\"Documentação do Hover Inspect\"
-                href=\"https://github.com/totvs/advpl-vscode/wiki/Debugando-e-executando#hover-inspect\">Documentação</a>)`});
+                href=\"https://github.com/totvs/advpl-vscode/issues/417\">Issue #417</a> | <a title=\"Documentação do Hover Inspect\"
+                href=\"https://github.com/totvs/advpl-vscode/wiki/Debugando-e-executando#hover-inspect\">Documentação</a>)`
+        });
+
         changeLog.push({
             kind: ChangeLogKind.NEW, message: `Adicionado suporte para <b>formatação de código</b> - (<a title=\"Open Issue #420\" 
-            href=\"https://github.com/totvs/advpl-vscode/pull/420\">PR #420</a>)`});
+                href=\"https://github.com/totvs/advpl-vscode/pull/420\">PR #420</a> | <a title=\"Documentação da Feature\"
+                href=\"https://github.com/totvs/advpl-vscode/wiki/Identação\">Documentação</a>)`
+        });
+
         changeLog.push({
             kind: ChangeLogKind.CHANGED, message: `Permitir <b>excluir um ambiente pelo gerenciador de ambientes</b> - (<a title=\"Open Issue #408\" 
-                href=\"https://github.com/totvs/advpl-vscode/issues/408\">Issue #408</a>)`});
+                href=\"https://github.com/totvs/advpl-vscode/issues/408\">Issue #408</a>)`
+        });
+
         changeLog.push({
-            kind: ChangeLogKind.CHANGED, message: `Adicionado <i>type</i> <code>Object</code> na sintaxe dos <b>atributos de classe</b>`});
+            kind: ChangeLogKind.CHANGED, message: `Adicionado <i>type</i> <code>Object</code> na sintaxe dos <b>atributos de classe</b>`
+        });
+
         changeLog.push({
             kind: ChangeLogKind.FIXED, message: `Ao compilar arquivos abertos, compilava a Workspace inteira - (<a title=\"Open Issue #411\" 
-            href=\"https://github.com/totvs/advpl-vscode/issues/411\">Issue #411</a>)</b>`});
+                href=\"https://github.com/totvs/advpl-vscode/issues/411\">Issue #411</a>)</b>`
+        });
+
         changeLog.push({
             kind: ChangeLogKind.FIXED, message: `JSON Bridge Return Invalid - (<a title=\"Open Issue #409\" 
-            href=\"https://github.com/totvs/advpl-vscode/issues/409\">Issue #409</a>)</b>`});
-            
+                href=\"https://github.com/totvs/advpl-vscode/issues/409\">Issue #409</a>)</b>`
+        });
+
         return changeLog;
     }
 
