783571b4acf97d2b04874b77f105cf903bb6dccf
Gabriel Alencar <pixcel97@gmail.com>
2021-10-13 16:36:35 -0300

    Tornado alpha_compile habilitado por default

--- src/whatsNew.ts
+++ src/whatsNew.ts
@@ -7,11 +7,12 @@ export class WhatsNewAdvPLContentProvider implements ContentProvider {
     public provideHeader(logoUrl: string): Header {
         return <Header>{
             logo: <Image>{ src: logoUrl, height: 25, width: 25 },
-            message: `<b>I Love AdvPL</b> provê os recursos necessários para edição, 
+            message: `<b>I Love AdvPL</b> provê os recursos necessários para edição,
                       compilação e debugging em AdvPL, 4GL e TL++ no Visual Code, além de recursos específicos
                       para gerenciamento de ambientes Protheus.`,
-            notice: `Recomenda-se em ambientes Windows habilitar a configuração <code>"advpl.alpha_compile": true</code> 
-                     pois as melhorias no Bridge (intermediador do VsCode com AppServer) estão sendo feitas 
+            notice: `Agora a configuração <code>"advpl.alpha_compile"</code> já vem habilitada por padrão.
+                     Caso a sua esteja como <code>false</code> recomendamos habilitar,
+                     pois as melhorias no Bridge (intermediador do VsCode com AppServer) estão sendo feitas
                      somente nessa versão.`};
     }
 
@@ -36,7 +37,7 @@ export class WhatsNewAdvPLContentProvider implements ContentProvider {
         //     link: "https://sponsorlink.codestream.com/?utm_source=vscmarket&utm_campaign=bookmarks&utm_medium=banner",
         //     image: "https://alt-images.codestream.com/codestream_logo_bookmarks.png",
         //     width: 35,
-        //     message: `<p>Discussing code is now as easy as highlighting a block and typing a comment right 
+        //     message: `<p>Discussing code is now as easy as highlighting a block and typing a comment right
         //               from your IDE. Take the pain out of code reviews and improve code quality.</p>`,
         //     extra:
         //         `<a title="Try CodeStream" href="https://sponsorlink.codestream.com/?utm_source=vscmarket&utm_campaign=bookmarks&utm_medium=banner">
